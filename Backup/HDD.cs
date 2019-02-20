785eb90de49f47d7ce37359d9d5b9f75d3fb1755
Diasss <47523360+Diasss@users.noreply.github.com>
2019-02-20 18:13:35 +0600

    Add files via upload

---
+++ Backup/HDD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup
+{
+  
+    class HDD:Storage
+    {
+        public HDD(double capacity) : base(capacity)
+        {
+
+        }
+        public override void CopyInfo() { }
+        public int speed { get; set; }
+        public int SectionsCount { get; set; }
+        public double sectionsVolue { get; set; }
+        public USBType USBType { get; set; }
+    }
+}
